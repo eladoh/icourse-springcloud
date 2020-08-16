7c3a3f058d9dfe2e37b82b1fc3b1906becc61ce8
hhe233 <58110129+hhe233@users.noreply.github.com>
2020-08-16 20:06:31 +0800

    1. eureka submodule as login service 2. change single maven to parent, child maven structure

--- eureka/src/main/java/com/htr/EurekaApplication.java
+++ eureka/src/main/java/com/htr/EurekaApplication.java
@@ -1,13 +1,13 @@
-package com.htr.icourse;
+package com.htr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IcourseApplication {
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IcourseApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
