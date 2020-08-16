e1416463b76fe8ff6f42bb483e44e90e251cbc2b
hhe233 <58110129+hhe233@users.noreply.github.com>
2020-08-16 19:41:12 +0800

    Spring initialization

---
+++ src/test/java/com/htr/icourse/IcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htr.icourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
