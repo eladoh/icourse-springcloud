dc47ae2d67d83d494f879f0f85735d2fcc985e03
hhe233 <58110129+hhe233@users.noreply.github.com>
2020-08-20 18:46:24 +0800

    1. add dto layer in server child module for controller and service layer 2. add ChapterDto.java 3. modified ChapterService, return ChapterDto instead of Chapter

---
+++ doc/db/all.sql
@@ -0,0 +1,12 @@
+
+drop table if exists `chapter`;
+create table `chapter` (
+  `id` char(8) not null comment 'id',
+  `course_id` char(8) comment '课程id',
+  `name` varchar(50) comment '名称',
+  primary key (`id`)
+
+) engine=innodb default charset=utf8mb4 comment='大章';
+
+insert into `chapter` (id, course_id, name) values ('00000001', '00000000', '测试大章01');
+insert into `chapter` (id, course_id, name) values ('00000002', '00000000', '测试大章02');
